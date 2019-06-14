1d6f099b515a18f1e8dd03e9eeadc1764410cd14
TJ Mahr <mahr@wisc.edu>
2019-06-14 12:55:39 -0500

    start unit test

--- tests/testthat/test-versus-old-aligner.R
+++ tests/testthat/test-versus-old-aligner.R
@@ -1,3 +1,29 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  those_eat_those_hotdogs_soon <- clean_old_alignment_result(
+    "dh-oU-z i-t dh-oU-z h-@-t--d-c-g-z  s-u-n"
+  )
+  hot_dogs <- clean_old_alignment_result(
+    "--------------------h-@-t  d-c-g-z------"
+  )
+
+  r <- align_phones(
+    those_eat_those_hotdogs_soon,
+    hot_dogs,
+    phone_match_aline
+  )
+
+  expect_equal(
+    r$a_alignment,
+    c("dh", "oU", "z", " ", "i", "t", " ", "dh", "oU", "z", " ",
+      "h", "@", "t", "-", "d", "c", "g", "z", " ", "s", "u", "n")
+  )
+
+  expect_equal(
+    r$b_alignment,
+    c("-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "h",
+      "@", "t", " ", "d", "c", "g", "z", "-", "-", "-", "-")
+  )
+
+
+
 })
