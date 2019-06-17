ed312f7c5b6c9a6309cefed4dd9562900d511ca2
TJ Mahr <mahr@wisc.edu>
2019-06-17 09:22:33 -0500

    snapshot

---
+++ R/benchmark.R
@@ -0,0 +1,8 @@
+
+read_alignment_file <- function() {
+  x <- "inst/data/a (7).txt"
+  lines <- readr::read_lines(x)
+  stop_here <- which(lines == "Consonants Confusion Matrix")
+  lines <- lines[seq(1, stop_here - 3)]
+  length(lines)
+}
